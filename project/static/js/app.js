44a1e025e0616e01ed342569151edfbc05c099a9
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-03-02 22:08:19 +0100

    added search function

---
+++ project/static/js/app.js
@@ -0,0 +1,81 @@
+// //imports 
+// import {
+//     getUserInput
+// } from './modules/userinput.js';
+
+// import {
+//     request
+// } from './modules/request.js';
+
+// import {
+//     render
+// } from './modules/render.js';
+
+// import {
+//     router
+// } from './modules/router.js';
+
+
+// //search form
+// const button = document.getElementById('submitbtn')
+// button.addEventListener("click", getUserInput);
+
+
+
+/*** Fetching data -> refactor into module later ***/
+const button = document.getElementById('submitbtn')
+button.addEventListener("click", getUserInput);
+
+function getUserInput() {
+  //Get the user input
+  const userInput = document.querySelector("input").value
+  console.log("Searching for: ", userInput)
+
+const main = document.querySelector('main');
+const cors = 'https://cors-anywhere.herokuapp.com/';
+const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
+const query = (userInput);
+const key = '1e19898c87464e239192c8bfe422f280';
+const secret = '4289fec4e962a33118340c888699438d';
+const detail = 'Default';
+
+
+const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+const config = {
+  Authorization: `Bearer ${secret}`
+};
+
+fetch(url, config)
+  .then(response => {
+    const Info = document.querySelector('main.list')
+    Info.innerHTML = ""
+    return response.json();
+  })
+  .then(data => {
+    render(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// render data
+function render(data) {
+  const results = data.results;
+  console.dir(results);
+  results.forEach((item, i) => {
+      const html = `
+            <article>
+              <img src="${
+                item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+              }">
+              <a href = '#${
+                item.id}'></a>
+            </article>
+            
+          `;
+      main.insertAdjacentHTML('beforeend', html);
+              })
+
+      
+}
+}
